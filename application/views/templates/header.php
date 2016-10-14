4d67984bc1ae6112a2f48a764e84000ba8dd9085
germanosk <germanobioinfo@gmail.com>
2016-10-13 23:42:26 -0400

    Adding favicon to the project! Yay we have a icon :D

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,6 +7,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Frello - Open Source Task Board</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="./icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="./icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="./icons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="./icons/manifest.json">
+    <link rel="mask-icon" href="./icons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="./icons/favicon.ico">
+    <meta name="msapplication-config" content="./icons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="./public/foundation/css/foundation.css">
     <link rel="stylesheet" href="./public/foundation/css/app.css">
   </head>
